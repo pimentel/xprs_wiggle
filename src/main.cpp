0996b2463bbf2c3c8b5edbae067053de90dfa312
Harold Pimentel <haroldpimentel@gmail.com>
2014-10-20 22:45:37 -0700

    add basic functionality with gff

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,26 @@
+#include <cstdio>
+
+#include <string>
 #include <iostream>
+
+#include "gff.h"
 
 
 int main(int argc, char *argv[])
 {
     std::cout << "Hello world!" << std::endl;
+    GffReader gff_reader;
+    const std::string fname = "/Users/hjp/lmcb/intron-retention/src/tests/inputs/refGene_07.23.2014_CHL1.gtf";
+    FILE* fhandle = fopen(fname.c_str(), "r");
+    gff_reader.init(fhandle, true);
+
+    std::cout << "here!" << std::endl;
+
+    gff_reader.readAll();
+
+    std::cout << "there!" << std::endl;
+
+    fclose(fhandle);
+
     return 0;
 }
