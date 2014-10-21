0aae488d23b7c50489966c9225763dd7823fa464
Harold Pimentel <haroldpimentel@gmail.com>
2014-10-21 11:23:09 -0700

    more boilerplate CMake. preparing for test suite

---
+++ src/example.cpp
@@ -0,0 +1,6 @@
+#include "example.h"
+
+double sq(double x)
+{
+    return x * x;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,15 @@
 #include <string>
 #include <iostream>
 
+#include "example.h"
+
 #include "gff.h"
 
 
 int main(int argc, char *argv[])
 {
     std::cout << "Hello world!" << std::endl;
+    std::cout << sq(3.0) << std::endl;
     GffReader gff_reader;
     const std::string fname = "/Users/hjp/lmcb/intron-retention/src/tests/inputs/refGene_07.23.2014_CHL1.gtf";
     FILE* fhandle = fopen(fname.c_str(), "r");
