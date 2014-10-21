21b57636ed3548c9823e642af205d1d3abb35756
Harold Pimentel <haroldpimentel@gmail.com>
2014-10-21 15:48:50 -0700

    add .ycm_extra_conf

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,24 @@
 
 #include "gff.h"
 
+void usage()
+{
+    std::cerr << "Usage: xprs_wiggle output.bam" << std::endl;
+}
 
 int main(int argc, char *argv[])
 {
+    if (argc != 2)
+    {
+        usage();
+        return 1;
+    }
     std::cout << "Hello world!" << std::endl;
     std::cout << sq(3.0) << std::endl;
-    GffReader gff_reader;
     const std::string fname = "/Users/hjp/lmcb/intron-retention/src/tests/inputs/refGene_07.23.2014_CHL1.gtf";
     FILE* fhandle = fopen(fname.c_str(), "r");
+
+    GffReader gff_reader;
     gff_reader.init(fhandle, true);
 
     std::cout << "here!" << std::endl;
